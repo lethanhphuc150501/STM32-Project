446f2144d940ccdfa51ed3cefa3da185d94a02a1
Phuc Le <129088655+lethanhphuc150501@users.noreply.github.com>
2023-07-25 09:47:21 +0700

    Update timer.c

--- CustomizableTrafficLight/Core/Src/timer.c
+++ CustomizableTrafficLight/Core/Src/timer.c
@@ -6,3 +6,14 @@
  */
 
 #include "timer.h"
+#include "main.h"
+#include "input_reading.h"
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  
+  if (htim->Instance == htim2)
+  {
+    button_reading();
+  }
+}
