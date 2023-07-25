4eb66e4b5c87e03679b4e5a73771c26af0bde828
Phuc Le <129088655+lethanhphuc150501@users.noreply.github.com>
2023-07-25 09:58:41 +0700

    Update input_processing.c

--- CustomizableTrafficLight/Core/Src/input_processing.c
+++ CustomizableTrafficLight/Core/Src/input_processing.c
@@ -4,5 +4,42 @@
  *  Created on: Jul 25, 2023
  *      Author: Phuc Le
  */
-#include "input_processing.h"
 
+#include "input_processing.h"
+#include "main.h"
+#include "input_reading.h"
+
+enum ButtonState{BUTTON_RELEASED, BUTTON_PRESSED, BUTTON_PRESSED_MORE_THAN_1_SECOND};
+enum ButtonState buttonState = BUTTON_RELEASED;
+
+void fsm_for_input_processing(void)
+{
+  switch(buttonState)
+  {
+    case BUTTON_RELEASED:
+      if (is_button_pressed(0))
+      {
+        buttonState = BUTTON_PRESSED;
+      }
+      break;
+    case BUTTON_PRESSED:
+      if (!is_button_pressed(0))
+      {
+        buttonState = BUTTON_RELEASED;
+      }
+      else
+      {
+        if (is_button_pressed_1s(0))
+        {
+          buttonState = BUTTON_PRESSED_MORE_THAN_1_SECOND;
+        }
+      }
+      break;
+    case BUTTON_PRESSED_MORE_THAN_1_SECOND:
+      if (!is_button_pressed(0))
+      {
+        buttonState = BUTTON_RELEASED;
+      }
+      break;
+  }
+}
