e7481cf52a4bb90da42f76363f33203c0fc68469
Phuc Le <129088655+lethanhphuc150501@users.noreply.github.com>
2023-07-25 09:42:02 +0700

    Update input_reading.h

--- CustomizableTrafficLight/Core/Inc/input_reading.h
+++ CustomizableTrafficLight/Core/Inc/input_reading.h
@@ -8,6 +8,8 @@
 #ifndef __INPUT_READING_H_
 #define __INPUT_READING_H_
 
-
+void button_reading(void);
+unsigned char is_button_pressed(unsigned char index);
+unsigned char is_button_pressed_1s(unsigned char index);
 
 #endif /* INC_INPUT_READING_H_ */
